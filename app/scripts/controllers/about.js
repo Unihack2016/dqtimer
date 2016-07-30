0bb166eccbcf34440cfa8258fda44c557fd40c86
Vatsalya Goel <vatsalyagoel@gmail.com>
2016-07-31 04:07:54 +1000

    Initial Commint yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dqtimerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dqtimerApp
+ */
+angular.module('dqtimerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dqtimerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dqtimerApp
+ */
+angular.module('dqtimerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
