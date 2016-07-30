2a7b4a8d4d67eaf6516a5c58e16466c90ac8b03e
Nick Wong <njwon4@gmail.com>
2016-07-31 09:02:43 +1000

    Added the points of interest screen

---
+++ app/scripts/controllers/interest.js
@@ -0,0 +1,4 @@
+angular.module('dqtimerApp')
+  .controller('InterestCtrl', ["$scope", "$routeParams", "$firebaseObject", "$timeout", function ($scope, $routeParams, $firebaseObject, $timeout) {
+      console.log("controller");
+  }]);
